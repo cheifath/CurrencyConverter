a89e27b02846265b85d6fa25c8ea68361423c2dc
Cheifath <cheifathmohammed@gmail.com>
2024-12-26 20:07:30 +0530

    Add temp files

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 const url = 'https://api.fxratesapi.com/latest?https://api.fxratesapi.com/latest?currencies=USD&base=INR=INR';
 
+for (codes in countryList) {
+   console.log(codes, countryList[codes]);
+}
